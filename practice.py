84136425647fdaafad5a95e17de671c98f87b3fe
Sean <seanskg72@gmail.com>
2021-01-31 23:09:07 +1100

    example

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-# atplotlib inline
+# Demo
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,9 +6,11 @@ import numpy as np
 x = np.arange(-3, 3, 0.01)
 
 plt.plot(x, norm.pdf(x))
-plt.savefig("yeet.png")
+## First graph
+plt.savefig("temp1.png")
 
 x = np.arange(-6, 6, 0.01)
 
+## Second graph
 plt.plot(x, norm.pdf(x))
-plt.savefig("temp.png")
+plt.savefig("temp2.png")
